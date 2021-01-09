df6a3f71e1387ec5883d0b63f420dfc6e3711ba2
jakbroda <brodaczz1@gmail.com>
2021-01-09 21:29:53 +0100

    tradeMarketDeals update

--- TradeSrednie/tradeMarketDeals.py
+++ TradeSrednie/tradeMarketDeals.py
@@ -6,13 +6,14 @@ from lib import CoinexPerpetualApi
 #from lib import request_client_coinex
 
 def tradeDeals(robot, vp, lim, percent, minVolume = 0, useCumulativePosition = False, cumulativePositionCount = 3):    
+    time.sleep(5)
     try:
         sellsPercentDeals = vp.getPercentDeals(lim, "sell")    
         buysPercentDeals = vp.getPercentDeals(lim, "buy")
         totalVolume = vp.getAmountDeals(lim)
 
         if(useCumulativePosition == True):
-            if(sellsPercentDeals > percent) and totalVolume > minVolume and vp.isBought == True and vp.buyPositions < cumulativePositionCount and time.time() - vp.lastBuyTime > 120 and vp.priceBought > vp.robot.get_pair_sell(vp.pair):
+            if(sellsPercentDeals > percent) and totalVolume > minVolume and vp.isBought == True and vp.buyPositions < cumulativePositionCount and time.time() - vp.lastBuyTime > 20 and vp.priceBought > vp.robot.get_pair_sell(vp.pair):
                 print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
                 vp.printDebug(robot.ORDER_DIRECTION_BUY)
                 
@@ -21,7 +22,7 @@ def tradeDeals(robot, vp, lim, percent, minVolume = 0, useCumulativePosition = F
                 
                 print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
                 
-            elif(buysPercentDeals > percent) and totalVolume > minVolume and vp.isSold == True and vp.sellPositions < cumulativePositionCount and time.time() - vp.lastSellTime > 120 and vp.priceSold < vp.robot.get_pair_buy(vp.pair):
+            elif(buysPercentDeals > percent) and totalVolume > minVolume and vp.isSold == True and vp.sellPositions < cumulativePositionCount and time.time() - vp.lastSellTime > 20 and vp.priceSold < vp.robot.get_pair_buy(vp.pair):
                 print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
                 vp.printDebug(robot.ORDER_DIRECTION_SELL)
                 
@@ -62,7 +63,7 @@ def tradeDeals(robot, vp, lim, percent, minVolume = 0, useCumulativePosition = F
         print(1)
         
 class ValuePair():
-    def __init__(self, _robot, _pair, _contractAmount, _isBought = False, _priceBought = 0, _isSold = False, _priceSold = 0):
+    def __init__(self, _robot, _pair, _contractAmount, _isBought = False, _priceBought = 0, _buyPositions = 0, _isSold = False, _priceSold = 0, _sellPositions = 0, _totalEarn = 0):
         self.robot = _robot
         self.realTrade = True
         self.pair = _pair
@@ -74,28 +75,41 @@ class ValuePair():
         self.sellPositions = 0
         self.lastBuyTime = time.time()
         self.lastSellTime = time.time()
-        self.totalEarn = 0 
+        self.totalEarn = _totalEarn 
+        self.realTotalEarn = 0
+        self.realTotalEarnWithFee = 0
         self.leverage = 10
         self.contractAmount = _contractAmount
 
     def closeShort(self):
         print("close " + self.pair + " short")
         pairBuy = self.robot.get_pair_buy(self.pair)
-        earn = (pairBuy - self.priceSold)/self.priceSold
-        earn = -earn
-        self.totalEarn += earn
-        self.totalEarn -= 0.0003
+        earn = (self.contractAmount*self.sellPositions)/(self.leverage*pairBuy) - (self.contractAmount*self.sellPositions)/(self.leverage*self.priceSold)
+        fee = 0.0005*((self.contractAmount*self.sellPositions)/(self.leverage*self.priceSold))
+        #earn = (pairBuy - self.priceSold)/self.priceSold
+        #earn = -earn
+        self.totalEarn += earn#*self.sellPositions
+        self.totalEarn -= fee
         print(earn)
+        print(fee)
         print(self.totalEarn)
+        print(self.realTotalEarn)
+        print(self.realTotalEarnWithFee)
 
     def closeLong(self):
         print("close " + self.pair + " long")
         priceSell = self.robot.get_pair_sell(self.pair)
-        earn = (priceSell - self.priceBought)/self.priceBought
-        self.totalEarn += earn
-        self.totalEarn -= 0.0003
+        earn = (self.contractAmount*self.buyPositions)/(self.leverage*priceSell) - (self.contractAmount*self.buyPositions)/(self.leverage*self.priceBought)
+        fee = 0.0005*((self.contractAmount*self.buyPositions)/(self.leverage*self.priceBought))
+        #earn = (priceSell - self.priceBought)/self.priceBought
+        earn = -earn
+        self.totalEarn += earn#*self.buyPositions
+        self.totalEarn -= fee
         print(earn)
+        print(fee)
         print(self.totalEarn)
+        print(self.realTotalEarn)
+        print(self.realTotalEarnWithFee)
 
     def resetAfterClose(self):
         self.isBought = False
@@ -162,14 +176,14 @@ class ValuePair():
 if __name__ == '__main__':
     robot = CoinexPerpetualApi()
 
-    vp1 = ValuePair(robot, 'BTCUSD', 100)
+    vp1 = ValuePair(robot, 'BTCUSD', 30)
     vp2 = ValuePair(robot, 'ETHUSD', 20)
 
     print(time.time())
 
     while True:
-        tradeDeals(robot, vp1, 20, 0.996)
-        tradeDeals(robot, vp2, 20, 0.993, minVolume=20000, useCumulativePosition=True, cumulativePositionCount=3)
+        tradeDeals(robot, vp1, 20, 0.95, minVolume=20000, useCumulativePosition=True, cumulativePositionCount=15)
+        #tradeDeals(robot, vp2, 20, 0.935, minVolume=5000, useCumulativePosition=True, cumulativePositionCount=10)
 
         
             
